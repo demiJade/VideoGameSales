14e541ac3880e65f57526bb8cf2588b16a833e26
demijade <sy_judy@yahoo.com>
2016-12-04 13:18:35 +0800

    added show sales in html using jade

---
+++ vgsales/routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/sales', function(req,res){
+	var MongoClient = mongodb.MongoClient;
+	var url = 'mongodb://localhost:27017/mytinydb';
+	MongoClient.connect(url,function(err,db){
+		if (err){
+			console.log("Unable to connect",err);
+		} else {
+			console.log('Connection established');
+			var collection = db.collection('sales');
+
+			collection.find({}).toArray(function(err,results){
+				if (err){
+					res.send(err);
+				} else if (results.length) {
+					res.render('sales',{
+						"sales":results
+					});
+				} else {
+					res.send('No documents found');
+				}
+				db.close();
+			});
+		}
+	});
+});
+
+
+
+module.exports = router;
